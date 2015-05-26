3abe6354968c31cb557b08f1b53868336e91e979
petitspois <qingdoufd@163.com>
2015-05-26 08:43:19 +0800

    add configureble

---
+++ src/config.js
@@ -0,0 +1,34 @@
+var structure＝ null,
+	delimiters = ['{{', '}}'];
+
+structure = {
+
+	/**
+	 * The prefix to look for when parsing directives.
+	 *
+	 * @type {String}
+	 */
+
+		preifx: 'rb-'
+
+	/**
+	 * Internal flag to indicate the delimiters have been changed.
+	 *
+	 * @type {Boolean}
+	 */
+
+		_delimitersChanged: true
+
+}
+
+Object.defineProperty(structure, 'delimiters', {
+	get:function(){
+		return delimiters;
+	},
+	set:function(newValue){
+		delimiters = newValue;
+		this._delimitersChanged = true;
+	}
+});
+
+module.exports = structure;
