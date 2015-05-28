832988a5c75ad91ca40c86bdb18edae90eacc7ce
petitspois <qingdoufd@163.com>
2015-05-28 16:59:26 +0800

    add utils env.js

---
+++ src/util/env.js
@@ -0,0 +1,103 @@
+/**
+ * Can we use __proto__?
+ *
+ * 存在__proto__?
+ *
+ * @type {Boolean}
+ */
+
+exports.hasProto = '__proto__' in {}
+
+/**
+ * Indicates we have a window
+ *
+ * @type {Boolean}
+ */
+
+var toString = Object.prototype.toString;
+var inBrowser = exports.inBrowser =
+	typeof window !== 'undefined' &&
+	toString.call(window) !== '[object Object]'
+
+/**
+ * Defer a task to execute it asynchronously. Ideally this
+ * should be executed as a microtask, so we leverage
+ * MutationObserver if it's available, and fallback to
+ * setTimeout(0).
+ *
+ * 视浏览器情况采用最快的异步回调
+ *
+ * @param {Function} cb
+ * @param {Object} ctx
+ */
+
+exports.nextTick = (function () {
+  var callbacks = []
+  var pending = false
+  var timerFunc
+  function handle () {
+    pending = false
+    var copies = callbacks.slice(0)
+    callbacks = []
+    for (var i = 0; i < copies.length; i++) {
+      copies[i]()
+    }
+  }
+  /* istanbul ignore if */
+  if (typeof MutationObserver !== 'undefined') {
+    var observer = new MutationObserver(handle)
+    var textNode = document.createTextNode('rebirth')
+    observer.observe(textNode, {
+      characterData: true
+    })
+    timerFunc = function () {
+      textNode.data = Math.random();
+    }
+  } else {
+    timerFunc = setTimeout
+  }
+  return function (cb, ctx) {
+    var func = ctx
+      ? function () { cb.call(ctx) }
+      : cb
+    callbacks.push(func)
+    if (pending) return
+    pending = true
+    timerFunc(handle, 0)
+  }
+})()
+
+/**
+ * Detect if we are in IE9...
+ *
+ * @type {Boolean}
+ */
+
+exports.ES5 =
+  inBrowser &&
+  window.dispatchEvent
+
+/**
+ * Sniff transition/animation events
+ */
+
+if (inBrowser && !exports.ES5) {
+  var isWebkitTrans =
+    window.ontransitionend === undefined &&
+    window.onwebkittransitionend !== undefined
+  var isWebkitAnim =
+    window.onanimationend === undefined &&
+    window.onwebkitanimationend !== undefined
+  exports.transitionProp = isWebkitTrans
+    ? 'WebkitTransition'
+    : 'transition'
+  exports.transitionEndEvent = isWebkitTrans
+    ? 'webkitTransitionEnd'
+    : 'transitionend'
+  exports.animationProp = isWebkitAnim
+    ? 'WebkitAnimation'
+    : 'animation'
+  exports.animationEndEvent = isWebkitAnim
+    ? 'webkitAnimationEnd'
+    : 'animationend'
+}
--- src/util/index.js
+++ src/util/index.js
@@ -1,1 +1,4 @@
-var lang = require('./lang');
+var lang = require('./lang'),
+	extend = lang.extend;
+
+extend(exports, lang);
